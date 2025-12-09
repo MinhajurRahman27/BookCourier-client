fcd0282380ac4a76e07b02fd3f1b752bef1c2192
Minhajur rahman <minhajurrahman2716@gmail.com>
2025-12-09 21:34:35 +0600

    Search system and sort system added

--- src/Pages/Book/Books.jsx
+++ src/Pages/Book/Books.jsx
@@ -1,26 +1,89 @@
 import { useQuery } from "@tanstack/react-query";
-import React from "react";
+import React, { useState } from "react";
 import useAxios from "../../Hooks/useAxios";
 import BookCard from "../../components/BookCard.jsx/BookCard";
 
 const Books = () => {
+  const [search, setSearch] = useState("");
+  const [allbooks, setAllbook] = useState([]);
   const axiosSecure = useAxios();
-  const { data: books = [] } = useQuery({
-    queryKey: ["allbook"],
+  const { data } = useQuery({
+    queryKey: ["allbook", search],
     queryFn: async () => {
-      const res = await axiosSecure.get("/allbooks");
-      return res.data;
+      const res = await axiosSecure.get(`/allbooks?searchText=${search}`);
+      return setAllbook(res.data);
     },
   });
+
+  console.log("all book:", allbooks);
+
+  const handleHighTolow = () => {
+    const sorted = [...allbooks].sort(
+      (a, b) => Number(b.price) - Number(a.price)
+    );
+
+    setAllbook(sorted);
+  };
+  const handlelowTohigh = () => {
+    const sorted = [...allbooks].sort(
+      (a, b) => Number(a.price) - Number(b.price)
+    );
+
+    setAllbook(sorted);
+  };
   return (
     <div>
-      book length: {books.length}
-     
-     <div className="grid grid-cols-4">
-       {books.map((book, index) => (
-        <BookCard key={index} book={book}></BookCard>
-      ))}
-     </div>
+      <div className="flex items-center justify-between px-10 pt-5">
+        <label className="input">
+          <svg
+            className="h-[1em] opacity-50"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+          >
+            <g
+              strokeLinejoin="round"
+              strokeLinecap="round"
+              strokeWidth="2.5"
+              fill="none"
+              stroke="currentColor"
+            >
+              <circle cx="11" cy="11" r="8"></circle>
+              <path d="m21 21-4.3-4.3"></path>
+            </g>
+          </svg>
+          <input
+            type="search"
+            onChange={(e) => setSearch(e.target.value)}
+            placeholder="Search"
+          />
+        </label>
+
+        <div className="dropdown ">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn border-gray-300 border m-1 w-31"
+          >
+            Click
+          </div>
+          <ul
+            tabIndex="-1"
+            className="dropdown-content menu  rounded-box z-1 w-33 "
+          >
+            <li className="">
+              <button onClick={handleHighTolow}>High to Low</button>
+            </li>
+            <li>
+              <button onClick={handlelowTohigh}>Low to High</button>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="grid grid-cols-4">
+        {allbooks.map((book, index) => (
+          <BookCard key={index} book={book}></BookCard>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/BookCard.jsx/BookCard.jsx
+++ src/components/BookCard.jsx/BookCard.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 
 const BookCard = ({ book }) => {
   console.log(book);
-  const { _id, bookname, bookimage, author } = book;
+  const { _id, bookname, bookimage, author, price } = book;
   return (
     <div className="card bg-base-100  sm:w-70 shadow-2xl shadow-indigo-500 hover:scale-105 transition delay-150 duration-300 ease-in-out mb-10  md:mb-0  md:m-10 ">
       <figure className="px-10 pt-10">
@@ -16,6 +16,7 @@ const BookCard = ({ book }) => {
       <div className="card-body ">
         <h2 className=" font-bold text-[18px] text-center">{bookname}</h2>
         <p className="text-center font-semibold text-gray-600">{author}</p>
+        <p className="text-center font-semibold text-gray-600">{price}</p>
         <div className="card-actions  flex justify-center">
           <Link
             to={`/bookdetails/${_id}`}
